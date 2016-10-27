99b57d65b9216ac38f9307659e29103296dfd5ed
Septian A Tama <email": null>
2016-10-27 19:35:09 +0700

    aaaa

---
+++ routes/oauth.js
@@ -0,0 +1,14 @@
+module.exports = {
+  FacebookAuth: {
+    appID: '572055026328061',
+    app_secret: '43725d82df6aebaaaf5c82bafbac1cf0'
+  },
+  TwitterAuth: {
+    consumerkey: 'JGQAqXnsO5R44pyAAQUfjskct',
+    consumerkey_secret: '12bbgRDvi3vkhfFFlXcGYWawJ1pLtGdB5FU6n2Gc1HcQqo940b',
+    callback: 'http://localhost:3000/twitter/callback'
+  },
+  GoogleAuth: {
+
+  }
+}
